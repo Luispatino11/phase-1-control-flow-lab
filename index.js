3e658002d57cd38a173e686d141a050b55d267ab
luis patino <lcp.patino@gmail.com>
2023-04-02 10:12:31 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let result;
+  if (feet <= 400) {
+    result = 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+    result = 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    result = 'No can do.';
+  }
+  return result;
+}
+console.log(scuberGreetingForFeet(500)); 
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
